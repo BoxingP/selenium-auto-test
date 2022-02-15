9c21a7d7eff4945cca09f7f2fa67ed401a9c8ad5
Boxing <boxingp@gmail.com>
2022-02-15 17:06:13 +0800

    SA-53: send alarm info of failed test cases.

--- lambda/parse_report/parse_report.py
+++ lambda/parse_report/parse_report.py
@@ -1,3 +1,11 @@
+import json
+import os
+import urllib.request
+from string import Template
+
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config.json')
+
+
 def generate_notification_msg(failed_tests: list):
     content = ''
     index = 1
@@ -40,6 +48,19 @@ def get_failed_tests(json_data):
     return failed_tests
 
 
+def send_alarm_info(failed_tests):
+    with open(CONFIG_PATH, 'r', encoding='UTF-8') as file:
+        config = json.load(file)
+    url = config['url']
+    alarm_template = Template(config['alarm_template'])
+    for test in failed_tests:
+        alarm = json.loads(alarm_template.substitute(target_name=test['name'], message=test['reason']))
+        params = json.dumps(alarm).encode('utf8')
+        req = urllib.request.Request(url, data=params, headers={'content-type': 'application/json'})
+        response = urllib.request.urlopen(req)
+
+
 def lambda_handler(event, context):
     failed_tests = get_failed_tests(event)
+    send_alarm_info(failed_tests)
     return generate_notification_msg(failed_tests)
