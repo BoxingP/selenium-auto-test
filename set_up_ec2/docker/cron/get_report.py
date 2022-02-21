a714f592746ed35e2c65393b6bc908e3329a56f0
Boxing <boxingp@gmail.com>
2022-02-21 14:55:47 +0800

    SA-59: only download recently updated allure reports files from s3.

--- set_up_ec2/docker/cron/get_report.py
+++ set_up_ec2/docker/cron/get_report.py
@@ -1,10 +1,16 @@
 import os
-import shutil
+from datetime import datetime, timezone, timedelta
 
 import boto3
 
 
-def download_files_from_s3(local_path, s3_bucket=os.environ['S3_BUCKET'], s3_directory=''):
+def download_files_from_s3(local_path, s3_bucket=os.environ['S3_BUCKET'], s3_directory='', within_days=None):
+    current = datetime.now(timezone.utc)
+    if within_days is None:
+        time_point = datetime.strptime('1970-01-01 00:00:00+00:00', "%Y-%m-%d %H:%M:%S%z")
+    else:
+        time_point = current - timedelta(days=within_days)
+
     if not os.path.exists(local_path):
         print('Making download directory "%s" ...' % local_path)
         os.makedirs(local_path)
@@ -15,23 +21,24 @@ def download_files_from_s3(local_path, s3_bucket=os.environ['S3_BUCKET'], s3_dir
         if 'Contents' in page:
             for obj in page['Contents']:
                 key = obj['Key']
+                last_modified = obj['LastModified']
                 if key[-1] == '/':
                     continue
                 absolute_path = os.path.join(local_path, *(key.split('/')[1:-1]))
                 if not os.path.exists(absolute_path):
                     os.makedirs(absolute_path)
-                client.download_file(Bucket=s3_bucket, Key=key,
-                                     Filename=os.path.join(absolute_path, key.split('/')[-1]))
+                file_name = os.path.join(absolute_path, key.split('/')[-1])
+                if last_modified > time_point:
+                    client.download_file(Bucket=s3_bucket, Key=key, Filename=file_name)
 
 
 def get_report():
     local_path = '/var/allure_reports'
     report_path = 'allure_reports/'
-    local_reports_path = os.path.join(local_path, report_path)
-
-    if os.path.exists(local_reports_path):
-        shutil.rmtree(local_reports_path)
-    download_files_from_s3(local_path, s3_directory=report_path)
+    days = None
+    if os.path.exists(local_path):
+        days = 1
+    download_files_from_s3(local_path, s3_directory=report_path, within_days=days)
 
 
 if __name__ == '__main__':
